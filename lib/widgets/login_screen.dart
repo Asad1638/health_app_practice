e18a2e3a1bdb08aef99322611a1d5227d4ce441d
Asad1638 <asadullah.soft@gmail.com>
2024-03-19 15:30:43 +0500

    ligin modified

--- lib/widgets/login_screen.dart
+++ lib/widgets/login_screen.dart
@@ -4,33 +4,37 @@ import 'package:flutter/material.dart';
 import 'package:health_app_2/widgets/screen-home.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.sizeOf(context).height;
-    double width = MediaQuery.sizeOf(context).width;
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: ClipRRect(
-        borderRadius: BorderRadius.circular(0),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 30,sigmaY: 30,tileMode: TileMode.mirror),
-          child: Container(
+      body: Stack(
+        children: [
+          Container(
             height: height,
             width: width,
             decoration: const BoxDecoration(
-                color: Colors.white,
-                image: DecorationImage(
-                    image: AssetImage("assets/images/image1.jpeg"),
-                    fit: BoxFit.fill)),
-            child: Center(
+              color: Colors.white,
+              image: DecorationImage(
+                image: AssetImage("assets/images/image1.jpeg"),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Center(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
               child: Container(
                 padding: EdgeInsets.symmetric(horizontal: width * 0.04),
                 height: height * 0.55,
                 width: height * 0.55,
                 decoration: BoxDecoration(
                   color: Colors.black.withOpacity(0.1),
-                  borderRadius: BorderRadius.circular(16.8),border: Border.all(color: Colors.white,width: 1.0),
+                  borderRadius: BorderRadius.circular(16.8),
+                  border: Border.all(color: Colors.white, width: 1.0),
                 ),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
@@ -42,9 +46,10 @@ class LoginScreen extends StatelessWidget {
                     Text(
                       "Login",
                       style: TextStyle(
-                          fontSize: height * 0.03,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w700),
+                        fontSize: height * 0.03,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
                     SizedBox(
                       height: height * 0.005,
@@ -57,25 +62,32 @@ class LoginScreen extends StatelessWidget {
                         color: Colors.white,
                       ),
                       decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.symmetric(horizontal: 0),
-                          labelText: " Enter Email ",
-                         labelStyle: TextStyle(
-                              fontSize: 12, color: Colors.white.withOpacity(0.5)),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Colors.white.withOpacity(0.3), width: 1.0),
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 0),
+                        labelText: " Enter Email ",
+                        labelStyle: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white.withOpacity(0.3),
+                            width: 1.0,
                           ),
-                          focusedBorder: const OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white, width: 1.0),
+                        ),
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white, width: 1.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white.withOpacity(0.3),
+                            width: 1.0,
                           ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Colors.white.withOpacity(0.3), width: 1.0),
-                          )),
+                        ),
+                      ),
                     ),
-          
+
                     //password text field
-          
+
                     SizedBox(
                       height: height * 0.02,
                     ),
@@ -87,76 +99,91 @@ class LoginScreen extends StatelessWidget {
                         color: Colors.white,
                       ),
                       decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.symmetric(horizontal: 0),
-                          labelText: " Enter Password ",
-                          labelStyle: TextStyle(
-                              fontSize: 12, color: Colors.white.withOpacity(0.5)),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Colors.white.withOpacity(0.3), width: 1.0),
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 0),
+                        labelText: " Enter Password ",
+                        labelStyle: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white.withOpacity(0.3),
+                            width: 1.0,
                           ),
-                          focusedBorder: const OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white, width: 1.0),
+                        ),
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white, width: 1.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.white.withOpacity(0.3),
+                            width: 1.0,
                           ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Colors.white.withOpacity(0.3), width: 1.0),
-                          )),
+                        ),
+                      ),
                     ),
-          
+
                     SizedBox(
                       height: height * 0.02,
                     ),
-          
+
                     const Align(
                       alignment: Alignment.centerRight,
                       child: Text(
                         "Forgot Password",
                         style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.black,
-                            fontWeight: FontWeight.w500),
+                          fontSize: 18,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
                     ),
-          
+
                     //button
                     SizedBox(
                       height: height * 0.04,
                     ),
                     ElevatedButton(
                       onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) =>const  HomeScreen(),));
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const HomeScreen()),
+                        );
                       },
-                      style: ElevatedButton.styleFrom(backgroundColor: Colors.black.withOpacity(0.1),
-                       ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black.withOpacity(0.1),
+                      ),
                       child: const Center(
                         child: Text(
                           "Login",
                           style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              color: Colors.white60
-                              ,
-                              fontSize: 16),
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white60,
+                            fontSize: 16,
+                          ),
                         ),
                       ),
                     ),
-          
+
                     SizedBox(
                       height: height * 0.016,
                     ),
-                   const    Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-Text("Don\'t have an account",style: TextStyle(fontSize: 15,color: Colors.black),)
-                ],
-              ),
+                    const Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don\'t have an account",
+                          style: TextStyle(fontSize: 15, color: Colors.black),
+                        )
+                      ],
+                    ),
                   ],
                 ),
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
