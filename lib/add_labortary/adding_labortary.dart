3259b251afce2c02159685c784f07697b05028e5
Asad1638 <asadullah.soft@gmail.com>
2024-04-06 00:14:46 +0500

    labortary

--- lib/add_labortary/adding_labortary.dart
+++ lib/add_labortary/adding_labortary.dart
@@ -2,6 +2,8 @@ import 'dart:ui';
 import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 class LaboratoryWidget extends StatefulWidget {
@@ -10,24 +12,46 @@ class LaboratoryWidget extends StatefulWidget {
 }
 
 class _LaboratoryWidgetState extends State<LaboratoryWidget> {
-  String laboratoryName = '';
-  String laboratoryAddress = '';
-  String id = '';
-  String password = '';
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+  final TextEditingController _idController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
-  void generateIdAndPassword() {
-    // Generate random ID
-    final Random random = Random();
-    final int randomNumber = random.nextInt(10000);
-    final String idPrefix = 'LAB';
-    id = '$idPrefix$randomNumber';
-
-    // Generate random password
-    const String chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    password =
-        List.generate(8, (index) => chars[random.nextInt(chars.length)]).join();
-
-    setState(() {});
+  Future<void> addLaboratoryToFirestore() async {
+    try {
+      await FirebaseFirestore.instance.collection('laboratories').add({
+        'name': _nameController.text,
+        'address': _addressController.text,
+        'id': _idController.text,
+        'password': _passwordController.text,
+      });
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+            child: AlertDialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              title: Text('Success'),
+              content: Text('Successfully added the laboratory.'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+              backgroundColor: Colors.white.withOpacity(0.7),
+            ),
+          );
+        },
+      );
+    } catch (error) {
+      print('Error adding laboratory: $error');
+    }
   }
 
   @override
@@ -43,86 +67,142 @@ class _LaboratoryWidgetState extends State<LaboratoryWidget> {
             height: MediaQuery.of(context).size.height,
             decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage(
-                    'assets/images/background_lab.jpeg'), // Update image path as needed
+                image: AssetImage('assets/images/background_lab.jpeg'),
                 fit: BoxFit.cover,
               ),
             ),
             child: BackdropFilter(
-              filter: ImageFilter.blur(
-                  sigmaX: 5, sigmaY: 5), // Adjust blur intensity as needed
+              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
               child: Container(
-                color:
-                    Colors.black.withOpacity(0.1), // Adjust opacity as needed
+                color: Colors.black.withOpacity(0.1),
               ),
             ),
           ),
           Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Laboratory Name',
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ), const SizedBox(height: 18,),
-                Padding(padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: TextFormField(
-                    onChanged: (value) {
-                      laboratoryName = value;
-                    },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      labelText: 'Enter Laboratory Name',
-                      labelStyle: const  TextStyle(color: Colors.black),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                 const  Center(child: Text("Laboratory Registration",style: TextStyle(fontSize: 18),)),SizedBox(height: 15,),
+          Padding(padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: TextFormField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        labelText: 'Enter Laboratory Name',
+                        labelStyle: const TextStyle(color: Colors.black),
+                      ),
+                      style: const TextStyle(color: Colors.black),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter laboratory name';
+                        }
+                        return null;
+                      },
                     ),
-                    style:const  TextStyle(color: Colors.black),
                   ),
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  'Laboratory Address',
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),const SizedBox(height: 18,),
-                Padding(padding:const EdgeInsets.symmetric(horizontal: 20),
-                  child: TextFormField(
-                    onChanged: (value) {
-                      laboratoryAddress = value;
-                    },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      labelText: 'Enter Laboratory Address',
-                      labelStyle: TextStyle(color: Colors.black),
+                  const SizedBox(height: 20),
+                  Padding(padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: TextFormField(
+                      controller: _addressController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        labelText: 'Enter Laboratory Address',
+                        labelStyle: const TextStyle(color: Colors.black),
+                      ),
+                      style: const TextStyle(color: Colors.black),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter laboratory address';
+                        }
+                        return null;
+                      },
                     ),
-                    style: TextStyle(color: Colors.black),
                   ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    generateIdAndPassword();
-                  },
-                  child: const Text(
-                    'Generate ID and Password',
+                  const SizedBox(height: 20),
+                  Padding(padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: TextFormField(
+                      controller: _idController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        labelText: 'Enter Laboratory ID',
+                        labelStyle: const TextStyle(color: Colors.black),
+                      ),
+                      style: const TextStyle(color: Colors.black),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter laboratory ID';
+                        }
+                        return null;
+                      },
+                    ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  'ID: $id',
-                  style: const TextStyle(fontSize: 16, color: Colors.black),
-                ),
-                Text(
-                  'Password: $password',
-                  style: const TextStyle(fontSize: 16, color: Colors.black),
-                ),
-              ],
+                  const SizedBox(height: 20),
+                  Padding(padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: TextFormField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        labelText: 'Enter Password',
+                        labelStyle: const TextStyle(color: Colors.black),
+                      ),
+                      style: const TextStyle(color: Colors.black),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter password';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_nameController.text.isNotEmpty &&
+                          _addressController.text.isNotEmpty &&
+                          _idController.text.isNotEmpty &&
+                          _passwordController.text.isNotEmpty) {
+                        addLaboratoryToFirestore();
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                              child: AlertDialog(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                title:const  Text('Error'),
+                                content:const  Text(
+                                    'Please fill all the required fields.'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text('OK'),
+                                  ),
+                                ],
+                                backgroundColor: Colors.white.withOpacity(0.65 ),
+                              ),
+                            );
+                          },
+                        );
+                      }
+                    },
+                    child: const Text('Confirm'),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
